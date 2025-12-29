c859d23a5ad125af4d6304b51870d3428dfba1b2
Julian Stephens <julian@julianstephens.net>
2025-12-29 16:13:37 -0500

    fix: simplify validatePDFContent

--- internal/pdf/reader.go
+++ internal/pdf/reader.go
@@ -2,6 +2,7 @@ package pdf
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -20,6 +21,76 @@ var (
 	config *pdfmodel.Configuration
 	once   sync.Once
 )
+
+type pdfSource interface {
+	Size() (int64, error)
+	ReadAt(p []byte, off int64) (n int, err error)
+}
+
+type filePDFSource struct {
+	path string
+}
+
+func (f *filePDFSource) Size() (int64, error) {
+	info, err := os.Stat(f.path)
+	if err != nil {
+		return 0, err
+	}
+	return info.Size(), nil
+}
+
+func (f *filePDFSource) ReadAt(p []byte, off int64) (n int, err error) {
+	file, err := os.Open(f.path)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+	return file.ReadAt(p, off)
+}
+
+type readerPDFSource struct {
+	rs io.ReadSeeker
+}
+
+func (r *readerPDFSource) Size() (int64, error) {
+	seeker, ok := r.rs.(io.Seeker)
+	if !ok {
+		return 0, errors.New("reader is not seekable")
+	}
+	currentPos, err := seeker.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return 0, fmt.Errorf("could not get current position: %w", err)
+	}
+	endPos, err := seeker.Seek(0, io.SeekEnd)
+	if err != nil {
+		return 0, fmt.Errorf("could not seek to end: %w", err)
+	}
+	_, err = seeker.Seek(currentPos, io.SeekStart)
+	if err != nil {
+		return 0, fmt.Errorf("could not restore position: %w", err)
+	}
+	return endPos, nil
+}
+
+func (r *readerPDFSource) ReadAt(p []byte, off int64) (n int, err error) {
+	seeker, ok := r.rs.(io.Seeker)
+	if !ok {
+		return 0, errors.New("reader is not seekable")
+	}
+	_, err = seeker.Seek(off, io.SeekStart)
+	if err != nil {
+		return 0, fmt.Errorf("could not seek to offset: %w", err)
+	}
+	n, err = r.rs.Read(p)
+	if err != nil {
+		return n, err
+	}
+	_, err = seeker.Seek(0, io.SeekStart)
+	if err != nil {
+		return n, fmt.Errorf("could not restore position: %w", err)
+	}
+	return n, nil
+}
 
 func detectEncryptionInTail(tail []byte) bool {
 	trailerIdx := bytes.LastIndex(tail, []byte("trailer"))
@@ -48,111 +119,43 @@ func validatePDFContent(filePath *string, reader *io.ReadSeeker) error {
 		return utils.NewErr("either filePath or reader must be provided", ErrInvalidPDF)
 	}
 
+	var source pdfSource
 	var fp string
+
 	if filePath != nil {
 		fp = *filePath
+		source = &filePDFSource{path: fp}
 	} else {
 		fp = "<reader>"
+		source = &readerPDFSource{rs: *reader}
 	}
 
-	var fileSize int64
-	var err error
-
-	if filePath != nil {
-		fileInfo, err := os.Stat(fp)
-		if err != nil {
-			return utils.WrapErr(fmt.Sprintf("could not stat file %q", fp), ErrInvalidPDF, err)
-		}
-		fileSize = fileInfo.Size()
-	} else {
-		if seeker, ok := (*reader).(io.Seeker); ok {
-			currentPos, err := seeker.Seek(0, io.SeekCurrent)
-			if err != nil {
-				return utils.WrapErr("could not get current position of reader", ErrInvalidPDF, err)
-			}
-			endPos, err := seeker.Seek(0, io.SeekEnd)
-			if err != nil {
-				return utils.WrapErr("could not seek to end of reader", ErrInvalidPDF, err)
-			}
-			_, err = seeker.Seek(currentPos, io.SeekStart)
-			if err != nil {
-				return utils.WrapErr("could not restore position of reader", ErrInvalidPDF, err)
-			}
-			fileSize = endPos
-		} else {
-			return utils.NewErr("reader is not seekable", ErrInvalidPDF)
-		}
+	fileSize, err := source.Size()
+	if err != nil {
+		return utils.WrapErr(fmt.Sprintf("could not determine size of %s", fp), ErrInvalidPDF, err)
 	}
 
 	if fileSize == 0 {
 		return utils.NewErr(fmt.Sprintf("file %q is empty", fp), ErrInvalidPDF)
 	}
 
-	var header = make([]byte, 8)
-	if filePath != nil {
-		file, err := os.Open(fp)
-		if err != nil {
-			return utils.WrapErr(fmt.Sprintf("could not open file %q", fp), ErrInvalidPDF, err)
-		}
-		defer file.Close()
-
-		_, err = file.Read(header)
-		if err != nil {
-			return utils.WrapErr(fmt.Sprintf("could not read file %q", fp), ErrInvalidPDF, err)
-		}
-	} else {
-		_, err = (*reader).Read(header)
-		if err != nil {
-			return utils.WrapErr("could not read from reader", ErrInvalidPDF, err)
-		}
-		_, err = (*reader).Seek(0, io.SeekStart)
-		if err != nil {
-			return utils.WrapErr("could not restore position of reader", ErrInvalidPDF, err)
-		}
+	header := make([]byte, 8)
+	if _, err := source.ReadAt(header, 0); err != nil {
+		return utils.WrapErr(fmt.Sprintf("could not read header of %s", fp), ErrInvalidPDF, err)
 	}
 
 	if !strings.HasPrefix(string(header), "%PDF-") {
 		return utils.NewErr(fmt.Sprintf("file %q does not appear to be a valid PDF", fp), ErrInvalidPDF)
 	}
 
-	var bufSize int64 = 1024
+	bufSize := int64(1024)
 	if fileSize < bufSize {
 		bufSize = fileSize
 	}
 
-	var trailer = make([]byte, bufSize)
-	if filePath != nil {
-		file, err := os.Open(fp)
-		if err != nil {
-			return utils.WrapErr(fmt.Sprintf("could not open file %q", fp), ErrInvalidPDF, err)
-		}
-
-		_, err = file.ReadAt(trailer, fileSize-bufSize)
-		if err != nil {
-			_ = file.Close()
-			return utils.WrapErr(fmt.Sprintf("could not read end of file %q", fp), ErrInvalidPDF, err)
-		}
-
-		if err := file.Close(); err != nil {
-			return utils.WrapErr(fmt.Sprintf("could not close file %q", fp), ErrInvalidPDF, err)
-		}
-	} else {
-		seeker, ok := (*reader).(io.Seeker)
-		if !ok {
-			return utils.NewErr("reader is not seekable", ErrInvalidPDF)
-		}
-		_, err = seeker.Seek(fileSize-bufSize, io.SeekStart)
-		if err != nil {
-			return utils.WrapErr("could not seek to end of reader", ErrInvalidPDF, err)
-		}
-		_, err = (*reader).Read(trailer)
-		if err != nil {
-			return utils.WrapErr("could not read from reader", ErrInvalidPDF, err)
-		}
-		_, err = seeker.Seek(0, io.SeekStart)
-		if err != nil {
-			return utils.WrapErr("could not restore position of reader", ErrInvalidPDF, err)
-		}
+	trailer := make([]byte, bufSize)
+	if _, err := source.ReadAt(trailer, fileSize-bufSize); err != nil {
+		return utils.WrapErr(fmt.Sprintf("could not read trailer of %s", fp), ErrInvalidPDF, err)
 	}
 
 	if !strings.Contains(string(trailer), "%%EOF") {
